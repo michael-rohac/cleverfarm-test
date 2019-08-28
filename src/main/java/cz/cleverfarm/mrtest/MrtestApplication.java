7a8fa8ce4f0f58b7a71c03aa58c489298bb4b293
Michal Rohac <michael.rohac@gmail.com>
2019-08-28 09:26:04 +0100

    Project scaffold using Spring Boot Initializr

---
+++ src/main/java/cz/cleverfarm/mrtest/MrtestApplication.java
@@ -0,0 +1,13 @@
+package cz.cleverfarm.mrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrtestApplication.class, args);
+	}
+
+}
